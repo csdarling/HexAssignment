f7d6ca00a9f119adfa2d146b202d8f7cc6e1cf2b
csd <csd@cathys-air.home>
2016-04-04 16:53:38 +0100

    Imported provided .java files to HexGame project

---
+++ src/hexgame/NoBoardDefinedException.java
@@ -0,0 +1,13 @@
+package hexgame;
+import java.lang.Exception;
+/**
+ * Exception to indicate that someone is trying to resize a board which is aready sized.
+ * 
+ * DO NOT CHANGE THIS FILE
+ * 
+ * @author Stephen McGough
+ * @version 0.1
+ */
+public class NoBoardDefinedException extends Exception
+{
+}
---
+++ src/hexgame/PositionAlreadyTakenException.java
@@ -0,0 +1,13 @@
+package hexgame;
+import java.lang.Exception;
+/**
+ * Exception to indicate that a cell where someone is trying to place a piece is already occupied
+ * 
+ * DO NOT CHANGE THIS FILE
+ *
+ * @author Stephen McGough
+ * @version 0.1
+ */
+public class PositionAlreadyTakenException extends Exception
+{
+}
